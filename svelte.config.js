35efc7472942682335d0a432f88d9d0b745c661e
ThingEngineer <josh.lee.campbell@gmail.com>
2023-11-02 11:50:39 -0500

    csrf prod test

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,8 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const isProd = process.env.NODE_ENV === 'production';
-console.log('isProd', isProd);
+// const isProd = process.env.NODE_ENV === 'production';
+// console.log('isProd', isProd);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -16,7 +16,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		csrf: isProd
+		// csrf: isProd
+		csrf: process.env.NODE_ENV !== 'development' // true in production, false in development
 	}
 };
 
