8be27afae97fa4cad2cf9f157d09727bdea5e4fa
ThingEngineer <josh.lee.campbell@gmail.com>
2023-11-02 11:37:11 -0500

    update and fix dev csrf

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,9 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const isProd = process.env.NODE_ENV === 'production';
+console.log('isProd', isProd);
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -12,7 +15,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		csrf: isProd
 	}
 };
 
